68b7d03cffa886398e463fd41255628de0c42171
Bálint Horváth <45066678+bvlnt@users.noreply.github.com>
2021-09-27 12:14:35 +0200

    putting the messages in an embed

--- main.py
+++ main.py
@@ -27,36 +27,33 @@ async def on_message(message):
     ##ide fognak jönni a különböző képek, amiken be lesz jelölve a kérdezett helység
     await message.channel.send("terkep")
 
-  if msg.startswith("!neptun"):
-    await message.channel.send(
-      "Működő Neptun bejelentkező linkek:"
-      "\n https://netw5.nnet.sze.hu/hallgato/login.aspx"
-      "\n https://netw6.nnet.sze.hu/hallgato/login.aspx"
-      "\n https://netw7.nnet.sze.hu/hallgato/login.aspx"
-      "\n https://netw8.nnet.sze.hu/hallgato/login.aspx"
-    )
-  
-  if msg.startswith("!gyujtoszamla"):
-    await message.channel.send(
-      "Számlaszám: 10300002-10801842-00024907"
-      "\n Számlát vezető pénzintézet: MKB Bank Nyrt"
-      "\n Számla tulajdonos: Széchenyi István Egyetem"
-      "\n Számla elnevezése: Neptun Hallgatói Gyűjtőszámla"
-      "\n\n Közleménybe a következőket írd:"
-      "\n NK-NEPTUN kód és szóközzel elválasztva a hallgató neve. Pl: NK-AB1234 Minta Péter"
-    )
+
+  if msg.startswith('!neptun'):
+    embedVar = discord.Embed(title="Működő Neptun linkek: ", description="", color=0x00ff00)
+    embedVar.add_field(name="Netw5:", value="https://netw5.nnet.sze.hu/hallgato/login.aspx", inline=False)
+    embedVar.add_field(name="Netw6:", value="https://netw6.nnet.sze.hu/hallgato/login.aspx", inline=False)
+    embedVar.add_field(name="Netw7:", value="https://netw7.nnet.sze.hu/hallgato/login.aspx", inline=False)
+    embedVar.add_field(name="Netw8:", value="https://netw8.nnet.sze.hu/hallgato/login.aspx", inline=False)
+    await message.channel.send(embed=embedVar)
 
 
-  if msg.startswith("!linkek"):
-    await message.channel.send(
-      "Hasznos linkek:"
-      "\n Tárgymátrix: https://sze.vortexcode.com"
-      "\n Kollégiumi adminisztrációk, jelentkezés: https://dormitory.sze.hu"
-      "\n HATÁR: https://user.sze.hu"
-      "\n Kollégiumi netvarázsló: https://internet.sth.sze.hu"
-      "\n MI/GIVK infós Discord: discord.gg/HhY5nEa"
-      "\n Működő Neptun linkekhez használd a '!neptun' parancsot!"
-    )
+  if msg.startswith('!gyujtoszamla'):
+    embedVar = discord.Embed(title="Gyüjtőszmálára való utalás menete: ", description="", color=0x00ff00)
+    embedVar.add_field(name="Számlaszám:", value="10300002-10801842-00024907", inline=False)
+    embedVar.add_field(name="Számlát vezető pénzintézet:", value="MKB Bank Nyrt", inline=False)
+    embedVar.add_field(name="Számla tulajdonos:", value="Széchenyi István Egyetem", inline=False)
+    embedVar.add_field(name="Közleménybe a következőket írd:", value="NK-NEPTUN kód és szóközzel elválasztva a hallgató neve. Pl: NK-AB1234 Minta Péter", inline=False)
+    await message.channel.send(embed=embedVar)
+
+  if msg.startswith('!linkek'):
+    embedVar = discord.Embed(title="Hasznos Linkek", description="", color=0x00ff00)
+    embedVar.add_field(name="Interaktív tárgymátrix:", value="https://sze.vortexcode.com", inline=False)
+    embedVar.add_field(name="Kollégiumi adminisztrációk:", value="https://dormitory.sze.hu", inline=False)
+    embedVar.add_field(name="HATÁR:", value="https://user.sze.hu", inline=False)
+    embedVar.add_field(name="Kollégiumi netvarázsló::", value="https://internet.sth.sze.hu", inline=False)
+    embedVar.add_field(name="MI/GIVK infós Discord:", value="discord.gg/HhY5nEa", inline=False)
+    embedVar.add_field(name="Neptun:", value="Működő Neptun linkekhez használd a '!neptun' parancsot!", inline=False)
+    await message.channel.send(embed=embedVar)
 
   if msg.startswith("!to"):
     await message.channel.send(
