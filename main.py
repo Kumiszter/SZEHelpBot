c60fdc28526859c64f867f6873f5de7239f5be1e
Bálint Horváth <45066678+bvlnt@users.noreply.github.com>
2021-09-27 09:15:43 +0200

    added new commands

--- main.py
+++ main.py
@@ -23,13 +23,50 @@ async def on_message(message):
     embed.set_image(url = "https://ehok.sze.hu/images/utmutatok/t%C3%A9rk%C3%A9p_feliratozott.png")  
     await message.channel.send("Az egyetem térképe: ",embed=embed)
 
+  if msg.startswith("hol van"):
+    ##ide fognak jönni a különböző képek, amiken be lesz jelölve a kérdezett helység
+    await message.channel.send("terkep")
+
   if msg.startswith("!neptun"):
     await message.channel.send(
-    "Működő Neptun bejelentkező linkek:"
-    "\n https://netw5.nnet.sze.hu/hallgato/login.aspx"
-    "\n https://netw6.nnet.sze.hu/hallgato/login.aspx"
-    "\n https://netw7.nnet.sze.hu/hallgato/login.aspx"
-    "\n https://netw8.nnet.sze.hu/hallgato/login.aspx"
+      "Működő Neptun bejelentkező linkek:"
+      "\n https://netw5.nnet.sze.hu/hallgato/login.aspx"
+      "\n https://netw6.nnet.sze.hu/hallgato/login.aspx"
+      "\n https://netw7.nnet.sze.hu/hallgato/login.aspx"
+      "\n https://netw8.nnet.sze.hu/hallgato/login.aspx"
+    )
+  
+  if msg.startswith("!gyujtoszamla"):
+    await message.channel.send(
+      "Számlaszám: 10300002-10801842-00024907"
+      "\n Számlát vezető pénzintézet: MKB Bank Nyrt"
+      "\n Számla tulajdonos: Széchenyi István Egyetem"
+      "\n Számla elnevezése: Neptun Hallgatói Gyűjtőszámla"
+      "\n\n Közleménybe a következőket írd:"
+      "\n NK-NEPTUN kód és szóközzel elválasztva a hallgató neve. Pl: NK-AB1234 Minta Péter"
+    )
+
+
+  if msg.startswith("!linkek"):
+    await message.channel.send(
+      "Hasznos linkek:"
+      "\n Tárgymátrix: https://sze.vortexcode.com"
+      "\n Kollégiumi adminisztrációk, jelentkezés: https://dormitory.sze.hu"
+      "\n HATÁR: https://user.sze.hu"
+      "\n Kollégiumi netvarázsló: https://internet.sth.sze.hu"
+      "\n MI/GIVK infós Discord: discord.gg/HhY5nEa"
+      "\n Működő Neptun linkekhez használd a '!neptun' parancsot!"
+    )
+
+  if msg.startswith("!to"):
+    await message.channel.send(
+      "https://to.sze.hu/kezdolap" ##itt akár kinyerhetnénk a különféle pultoknál hányadik sorszám van (persze ha újra működne, 2020 óta nem volt frissülve)
+    )
+  
+  if msg.startswith("!datumok"):
+    await message.channel.send(
+      "Fontos dátumok"
+      ##ide valahogy le kéne hívni a tanszüneti dátumokat, jelenlegi vizsgaidőszak kezdete időpontját...
     )
 
 keep_alive()
