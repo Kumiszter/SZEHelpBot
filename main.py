eca91e99845cc5cc96b8015bc9f5d7ecf7b5cb1c
Márton Unger <57155257+Kumiszter@users.noreply.github.com>
2021-09-29 14:52:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import discord
 import os
 import requests 
 import json
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import lxml
 from keep_alive import keep_alive
 
 client = discord.Client()
 
 embed = discord.Embed()  
+
 
 @client.event
 async def on_ready():
@@ -20,8 +25,9 @@ async def on_message(message):
   msg = message.content
 
   if msg.startswith("!terkep"):
-    embed.set_image(url = "https://ehok.sze.hu/images/utmutatok/t%C3%A9rk%C3%A9p_feliratozott.png")  
-    await message.channel.send("Az egyetem térképe: ",embed=embed)
+    embedVar = discord.Embed(title="SZE Térkép", description="", color=0x00ff00)
+    embedVar.set_image(url="https://cdn.discordapp.com/attachments/255085444507762688/892750999729627177/terkep.png")
+    await message.channel.send(embed=embedVar)
 
   if msg.startswith("hol van"):
     ##ide fognak jönni a különböző képek, amiken be lesz jelölve a kérdezett helység
@@ -52,19 +58,28 @@ async def on_message(message):
     embedVar.add_field(name="HATÁR:", value="https://user.sze.hu", inline=False)
     embedVar.add_field(name="Kollégiumi netvarázsló::", value="https://internet.sth.sze.hu", inline=False)
     embedVar.add_field(name="MI/GIVK infós Discord:", value="discord.gg/HhY5nEa", inline=False)
+    embedVar.add_field(name="SzocTám KisOkos:", value="SzocTám KisOkoshoz használd a '!szoctam' parancsot!", inline=False)
     embedVar.add_field(name="Neptun:", value="Működő Neptun linkekhez használd a '!neptun' parancsot!", inline=False)
     await message.channel.send(embed=embedVar)
 
   if msg.startswith("!to"):
-    await message.channel.send(
-      "https://to.sze.hu/kezdolap" ##itt akár kinyerhetnénk a különféle pultoknál hányadik sorszám van (persze ha újra működne, 2020 óta nem volt frissülve)
-    )
+    df_list = pd.read_html('https://to.sze.hu/kezdolap', header=0)
+    df = df_list[0]
+    await message.channel.send(df)
+    ##szépíteni!!!
+
   
   if msg.startswith("!datumok"):
     await message.channel.send(
       "Fontos dátumok"
       ##ide valahogy le kéne hívni a tanszüneti dátumokat, jelenlegi vizsgaidőszak kezdete időpontját...
     )
+
+  if msg.startswith("!szoctam"):
+    embedVar = discord.Embed(title="SzocTám KisOkos", description="", color=0x00ff00)
+    embedVar.add_field(name="Szociális támogatás igénylésést segítő KisOkos:", value="https://kollegium.sze.hu/images/Határ%20segédlet/SzocTám%20kisokos%20elsőéves_kollégiumi%20jelentkezéshez.pdf", inline=False)
+    embedVar.set_image(url="https://hok.uni-obuda.hu/uploads/File/almasir/makeItRain.jpg")
+    await message.channel.send(embed=embedVar)
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
